f79608fcb9e4572d8d34cb848f04bb8f1b9f77fe
Peter Stein <peterstein@protonmail.com>
2020-09-28 01:51:27 +0200

    Fix tests

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -4,9 +4,9 @@ from audiocards.audio import text_to_speech
 
 
 def test_text_to_speech():
-    filepath = pathlib.Path("data/audio/ru_бутерброд.mp3")
+    filepath = pathlib.Path("test_data/audio/ru_бутерброд.mp3")
     assert not filepath.exists()
-    text_to_speech(text="Бутерброд", language="ru", path="data/audio/")
+    text_to_speech(text="Бутерброд", language="ru", path="test_data/audio/")
     assert filepath.exists()
-    text_to_speech(text="Бутерброд", language="ru", path="data/audio/")
+    text_to_speech(text="Бутерброд", language="ru", path="test_data/audio/")
     filepath.unlink()
