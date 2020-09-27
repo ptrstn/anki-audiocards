84a95757b1ab28a6a8f60781a0963bdb962bd3f6
Peter Stein <peterstein@protonmail.com>
2020-09-27 23:35:33 +0200

    Create filename module

---
+++ audiocards/filename.py
@@ -0,0 +1,25 @@
+import re
+import unicodedata
+
+
+def slugify(text):
+    """
+    Borrowed from https://docs.djangoproject.com/en/3.0/_modules/django/utils/text/#slugify
+
+    Convert spaces to underscores.
+    Remove characters that aren't alphanumerics, underscores, or hyphens.
+    Convert to lowercase.
+    Strip leading and trailing whitespace.
+    """
+    text = unicodedata.normalize('NFKC', text)
+    text = re.sub(r'[^\w\s-]', '', text).strip().lower()
+    return re.sub(r'[-\s]+', '_', text)
+
+
+def text_to_filename(text, suffix=None, prefix=None):
+    filename = slugify(text)
+    if suffix:
+        filename = f"{filename}{suffix}"
+    if prefix:
+        filename = f"{prefix}{filename}"
+    return filename
