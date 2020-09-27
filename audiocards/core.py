4712f2b0bf0280fdbd318edbaed50846f1e60088
Peter Stein <peterstein@protonmail.com>
2020-09-28 01:52:18 +0200

    Move deck generation script in reusable function

--- audiocards/core.py
+++ audiocards/core.py
@@ -5,20 +5,24 @@ from audiocards.audio import text_to_speech
 from audiocards.filename import text_to_filename
 from audiocards.flashcards import generate_model, generate_deck, generate_note
 
-language = "ru"
 
-df = pandas.read_csv("data/vokabeln.csv", sep="\t")
-df.iloc[:, 0].apply(text_to_speech, language=language, path="data/audio")
+def create_deck(csv_path, language, deck_name):
+    language = language
 
-field_names = list(df)
-my_model = generate_model(model_name="ru", field_names=field_names)
-my_deck = generate_deck(name="Russisch für Anfänger")
+    df = pandas.read_csv(csv_path, sep="\t")
+    df.iloc[:, 0].apply(text_to_speech, language=language, path="data/audio")
 
-df.loc[:, "audiofile"] = df.iloc[:, 0].apply(
-    text_to_filename, prefix=f"{language}_", suffix=".mp3"
-)
-df.apply(lambda row: my_deck.add_note(generate_note(my_model, row.values)), axis=1)
+    field_names = list(df)
+    my_model = generate_model(model_name=language, field_names=field_names)
+    my_deck = generate_deck(name=deck_name)
 
-my_package = genanki.Package(my_deck)
-my_package.media_files = [f"data/audio/{file}" for file in list(df.audiofile)]
-my_package.write_to_file("russisch.apkg")
+    df.loc[:, "audiofile"] = df.iloc[:, 0].apply(
+        text_to_filename, prefix=f"{language}_", suffix=".mp3"
+    )
+    df.apply(lambda row: my_deck.add_note(generate_note(my_model, row.values)), axis=1)
+
+    my_package = genanki.Package(my_deck)
+    my_package.media_files = [f"data/audio/{file}" for file in list(df.audiofile)]
+    package_path = f"{text_to_filename(deck_name)}.apkg"
+    my_package.write_to_file(package_path)
+    print(f"Generated deck in {package_path}")
