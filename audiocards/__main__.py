5c29161e9279bf775b00f268203ab75bec1bef56
Peter Stein <peterstein@protonmail.com>
2020-09-28 02:03:10 +0200

    Update main script to generate decks on CLI

--- audiocards/__main__.py
+++ audiocards/__main__.py
@@ -1,22 +1,32 @@
 import argparse
 
 from audiocards import __version__
+from audiocards.core import create_deck
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(
-        description="Replace this text with your description"
-    )
+    parser = argparse.ArgumentParser(description="An Anki audio flash card generator")
 
     parser.add_argument(
         "--version", action="version", version="%(prog)s {}".format(__version__)
     )
 
+    parser.add_argument("csv_path", help="Path to the csv file")
+    parser.add_argument("language", help="Language of flash cards")
+    parser.add_argument("deck_name", help="Name of the Anki deck")
+
     return parser.parse_args()
 
 
 def main():
-    print("Hello there.")
+    args = parse_arguments()
+    print(args)
+    print(args.csv_path)
+    print(args.language)
+    print(args.deck_name)
+    create_deck(
+        csv_path=args.csv_path, language=args.language, deck_name=args.deck_name
+    )
 
 
 if __name__ == "__main__":
--- tests/test_flashcards.py
+++ tests/test_flashcards.py
@@ -2,6 +2,6 @@ from audiocards.flashcards import create_fields_list
 
 
 def test_create_fields_list():
-    names = ["russisch", "deutsch" ]
+    names = ["russisch", "deutsch"]
     expected = [{"name": "russisch"}, {"name": "deutsch"}, {"name": "Audio"}]
     assert create_fields_list(names) == expected
