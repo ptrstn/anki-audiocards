6b1f01dcf3ba008d2cd2f8a47ba30b200f8ea2d0
Peter Stein <peterstein@protonmail.com>
2020-09-28 02:30:49 +0200

    Increase test coverage

---
+++ tests/test_core.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from audiocards.core import create_deck
+
+
+def test_create_deck():
+    csv_path = "data/vokabeln.csv"
+    deck_name = "Test::Russischkurs::VL1"
+    language = "ru"
+
+    expected_package_path = Path("test__russischkurs__vl1.apkg")
+    assert not expected_package_path.exists()
+    create_deck(csv_path=csv_path, language=language, deck_name=deck_name)
+    assert expected_package_path.exists()
+    expected_package_path.unlink()
