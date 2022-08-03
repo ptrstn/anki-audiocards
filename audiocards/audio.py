7c8359570a9f104153285e9a5a44aebad7cb9fdf
Peter Stein <peterstein@protonmail.com>
2022-08-03 20:50:33 +0200

    Add docstring to text_to_speech function

--- audiocards/audio.py
+++ audiocards/audio.py
@@ -6,6 +6,14 @@ from audiocards.filename import text_to_filename
 
 
 def text_to_speech(text, language, path, skip_exist=True):
+    """
+    :param text: Text to be voiced
+    :param language: Language of the text
+    :param path: Path to the directory of the mp3 file
+    :param skip_exist: Skip if mp3 file already exists
+    :return:
+    """
+
     filename = text_to_filename(text, prefix=f"{language}_", suffix=".mp3")
     filepath = pathlib.Path(path, filename)
     filepath.parent.mkdir(exist_ok=True, parents=True)
